627c63e92f1a201c2ad8f94aca3c03fb586d8ab0
ian-sim <i.simpson8891@gmail.com>
2020-05-01 15:51:36 +0800

    adding code for t-shirt section

--- interactive-form-v2/js/script.js
+++ interactive-form-v2/js/script.js
@@ -16,3 +16,49 @@ titleDropdown.addEventListener("change", () => {
 		otherTitle.style.display = "none";
 	}
 });
+
+// Create function to hide element
+function hideElement(elementName) {
+	elementName.style.display = "none";
+}
+
+// Create function to hide multiple elements
+function hideMultiple(elementList) {
+	for (let i = 0; i < elementList.length; i++) {
+		hideElement(elementList[i]);
+	}
+}
+
+// Create 'please select' option for color dropdown
+const colorDropdown = document.querySelector("#color");
+const selectThemeMessage = document.createElement("option");
+selectThemeMessage.selected = true;
+selectThemeMessage.textContent = "Please select a theme";
+const firstColorOption = colorDropdown.firstElementChild;
+colorDropdown.insertBefore(selectThemeMessage, firstColorOption);
+
+// Hide color dropdown options initially
+hideMultiple(colorDropdown);
+
+// Create listener for design drop down selection
+const designDropdown = document.querySelector("#design");
+designDropdown.addEventListener("change", () => {
+	hideMultiple(colorDropdown);
+	if (designDropdown.value === "js puns") {
+		selectThemeMessage.hidden = true;
+		for (let i = 0; i < colorDropdown.length; i++) {
+			if (i < 4) {
+				colorDropdown[i].style.display = "inherit";
+			}
+		}
+	} else if (designDropdown.value === "heart js") {
+		selectThemeMessage.hidden = true;
+		for (let i = 0; i < colorDropdown.length; i++) {
+			if (i >= 4) {
+				colorDropdown[i].style.display = "inherit";
+			}
+		}
+	} else {
+		selectThemeMessage.hidden = false;
+	}
+});
